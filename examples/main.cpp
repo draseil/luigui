690bb1bd9b87beff13fc5bcd60fda0e9e51c0c37
liesard <makarafreak@gmail.com>
2016-07-29 12:23:11 +0200

    Example now creates an empty window

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/gl.h>
 #include "entity.hpp"
+#include <GLFW/glfw3.h>
+
+constexpr int WIDTH = 640, HEIGHT = 480;
+
+GLFWwindow *createWindow() {
+    GLFWwindow *window = NULL;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "luigui example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    return window;
+}
 
 int main() {
+    glfwInit();
+    GLFWwindow *window = createWindow();
+    glewExperimental = GL_TRUE;
+    glewInit();
+
     Entity e({1, 2}, {3, 4});
 
     std::cout << e.getPosition().x << ", " << e.getPosition().y << std::endl
               << e.getSize().x     << ", " << e.getSize().y     << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
 }
